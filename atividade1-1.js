5d67aac1a9e955088191ca965d9ff1b013540194
magalhaes <magalhaes@neurastream.com>
2020-02-17 19:31:12 -0300

    atividade 1 editada

--- atividade1-1.js
+++ atividade1-1.js
@@ -1,10 +1,16 @@
 var user = require('readline-sync'); //importa a biblioteca readline-sync na variavel user
 
-var resultadoDoUsuario = user.question("Qual a sua idade? "); //chamada método question da biblioteca readline-sync
+var resultadoDoUsuario = user.question("Qual ano você nasceu? "); //chamada método question da biblioteca readline-sync
+var anoAtual = "2020"
 
-if (resultadoDoUsuario >= 16){
+if (anoAtual - resultadoDoUsuario <'16'){
+    console.log("Usuário não pode votar!")
+} else if (anoAtual - resultadoDoUsuario >= '16' && anoAtual - resultadoDoUsuario < '18'){
+    console.log("Usuário Pode votar!")
+}else if (anoAtual - resultadoDoUsuario >= '18' && anoAtual - resultadoDoUsuario < '65'){
+    console.log("Usuário é obrigado a votar!")
+}else{
     console.log("Usuario pode votar!")
-} else{
-    console.log("Usuario não pode votar!")
 }
 
+console.log(anoAtual - resultadoDoUsuario + " anos")
